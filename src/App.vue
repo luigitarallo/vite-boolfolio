6ac0eee5b5d8e3c7ccdaab0d7691fca8a7d4d26f
luigitarallo <tarallo.gigi@gmail.com>
2023-11-04 19:53:53 +0100

    feat: add axios requesto to get data from api

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+import ProjectCard from "./components/projects/ProjectCard.vue";
+import axios from "axios";
+
 export default {
   data() {
-    return {};
+    return {
+      projects: [],
+      api: {
+        baseUrl: "http://localhost:8000/api/",
+      },
+    };
+  },
+  components: { ProjectCard },
+
+  methods: {
+    fetchProjects(uri = this.api.baseUrl + "projects") {
+      axios.get(uri).then((response) => {
+        this.projects = response.data.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchProjects();
   },
 };
 </script>
 
-<template><h1>Hello World!</h1></template>
+<template>
+  <h1>Hello World!</h1>
+  <ProjectCard />
+</template>
 
 <style lang="scss"></style>
