3e853ae7fc9c26c0146cbc58b19bb421575ed350
luigitarallo <tarallo.gigi@gmail.com>
2023-11-04 18:48:49 +0100

    feat: import bootstrap and scss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./assets/scss/style.scss";
 
-createApp(App).mount('#app')
+import * as bootstrap from "bootstrap";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
