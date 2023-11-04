187e1331e19773064e6cb2f2bf7853c8d9d7cd42
luigitarallo <tarallo.gigi@gmail.com>
2023-11-04 19:53:21 +0100

    feat: add component ProjectCard

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "My Projects",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
