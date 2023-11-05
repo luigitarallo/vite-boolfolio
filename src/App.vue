247c76707c0a98bcae34cba4497a4cdf924ad292
luigitarallo <tarallo.gigi@gmail.com>
2023-11-05 18:31:14 +0100

    feat: add axios request for page links and v-for

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,14 @@ import axios from "axios";
 export default {
   data() {
     return {
+      // Array vuoto iniziale dove salvare i dati ricevuti dalla richiesta axios
       projects: [],
+      // Url base che contiene le api generate su laravel
       api: {
         baseUrl: "http://localhost:8000/api/",
       },
+      // Oggetto vuoto iniziale che conterrà i link delle pagine
+      pagination: { links: null },
     };
   },
   components: { ProjectCard },
@@ -17,6 +21,7 @@ export default {
     fetchProjects(uri = this.api.baseUrl + "projects") {
       axios.get(uri).then((response) => {
         this.projects = response.data.data;
+        this.pagination.links = response.data.links;
       });
     },
   },
@@ -28,11 +33,22 @@ export default {
 </script>
 
 <template>
-  <div class="container">
+  <div class="container my-3">
     <h1>Luigi's Projects</h1>
-    <div class="row rows-cols-4 g-3">
+    <div class="row row-cols-4 g-3">
       <ProjectCard v-for="project in projects" :project="project" />
     </div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination my-2">
+        <li
+          @click="fetchProjects(link.url)"
+          v-for="link in pagination.links"
+          class="page-item"
+        >
+          <a class="page-link" href="#" v-html="link.label"></a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
