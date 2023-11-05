6682ece28147e4eae2720f5f40aff1f96659d8cb
luigitarallo <tarallo.gigi@gmail.com>
2023-11-05 18:33:30 +0100

    feat: add card with dynamic data gotten from props

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -4,7 +4,7 @@ export default {
     return {};
   },
   props: {
-    project: Array,
+    project: Object,
   },
 };
 </script>
@@ -12,15 +12,38 @@ export default {
 <template>
   <div class="col">
     <div class="card h-100" style="width: 18rem">
+      <div class="card-header text-bg-primary py-2">
+        {{ project.name }}
+      </div>
       <div class="card-body">
-        <h5 class="card-title">Name: {{ project.name }}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">
-          <strong>Link: </strong
-          ><a href="{{ project.link }}">{{ project.link }}</a>
+        <strong>Type: </strong>
+        <span
+          v-if="project.type"
+          class="badge"
+          :style="{ backgroundColor: project.type.color }"
+        >
+          {{ project.type.name }}
+        </span>
+        <span class="badge text-bg-warning" v-else>Not defined</span>
+
+        <h6 class="card-text mb-2">
+          <br />
+          <strong>Technologies: </strong> <br />
+          <span
+            v-if="project.technologies.length > 0"
+            v-for="technology in project.technologies"
+            class="badge rounded-pill m-1"
+            :style="{ backgroundColor: technology.color }"
+            >{{ technology.name }}</span
+          >
+          <span v-else class="badge rounded-pill text-bg-info m-1"
+            >Not defined</span
+          >
         </h6>
         <p class="card-text">
           <strong>Description: </strong>{{ project.description }}
         </p>
+        <a href="{{ project.link }}">Link</a>
       </div>
     </div>
   </div>
