533699007b9fe7bbae345e4bb6561448136d48b6
luigitarallo <tarallo.gigi@gmail.com>
2023-11-05 11:34:59 +0100

    add: props, bootstrap card classes

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,15 +1,29 @@
 <script>
 export default {
   data() {
-    return {
-      title: "My Projects",
-    };
+    return {};
+  },
+  props: {
+    project: Array,
   },
 };
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <div class="col">
+    <div class="card h-100" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">Name: {{ project.name }}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">
+          <strong>Link: </strong
+          ><a href="{{ project.link }}">{{ project.link }}</a>
+        </h6>
+        <p class="card-text">
+          <strong>Description: </strong>{{ project.description }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
