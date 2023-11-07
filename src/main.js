5be35d1ae2e74391e24a5d4c3b1967d17b81cd3e
luigitarallo <tarallo.gigi@gmail.com>
2023-11-07 20:20:57 +0100

    feat: add import and use router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import "./assets/scss/style.scss";
 
+import { router } from "./router";
+
 import * as bootstrap from "bootstrap";
 
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
