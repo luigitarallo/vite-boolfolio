797722e35cc4c086f0ce3083575a566a56209aff
luigitarallo <tarallo.gigi@gmail.com>
2023-11-07 20:19:30 +0100

    feat: add routes to homepage and portfolio

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import PortfolioPage from "../pages/PortfolioPage.vue";
+import ProjectDetailPage from "../pages/ProjectDetailPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "homepage",
+      component: HomePage,
+    },
+    {
+      path: "/porfolio",
+      name: "porfolio",
+      component: PortfolioPage,
+    },
+  ],
 });
 export { router };
