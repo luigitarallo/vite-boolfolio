19ab1c52f5f636199e600d23f1c87ad7e1ab3e2c
luigitarallo <tarallo.gigi@gmail.com>
2023-11-08 23:45:25 +0100

    feat: add page with filtered projects

---
+++ src/pages/FilteredByTypePage.vue
@@ -0,0 +1,13 @@
+<script>
+import ProjectTypeList from "../components/projects/ProjectTypeList.vue";
+
+export default {
+  components: { ProjectTypeList },
+};
+</script>
+
+<template>
+  <ProjectTypeList />
+</template>
+
+<style lang="scss" scoped></style>
